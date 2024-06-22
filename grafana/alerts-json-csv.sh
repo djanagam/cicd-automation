a6c9b7d83184d4811c615bc02bb028a929105760
djanagam <92891623+djanagam@users.noreply.github.com>
2024-06-22 16:45:10 -0400

    Update alerts-json-csv.sh

--- grafana/alerts-json-csv.sh
+++ grafana/alerts-json-csv.sh
@@ -1,5 +1,20 @@
-jq -r '
-[.[] | {id, name, condition, frequency}] | 
-(first | keys_unsorted) as $keys | 
-$keys, map([.[ $keys[] ]])[] | @csv
+jq.exe -r '
+[
+  .[] 
+  | .folder as $folder 
+  | .dashboards[] 
+  | .dashboard as $dashboard 
+  | .alerts[] 
+  | {
+      folder: $folder,
+      dashboard: $dashboard,
+      id: .id,
+      name: .name,
+      condition: .condition,
+      frequency: .frequency
+    }
+] 
+| (first | keys_unsorted) as $keys 
+| $keys, map([.[ $keys[] ]])[] 
+| @csv
 ' alert_rules.json > alert_rules.csv
