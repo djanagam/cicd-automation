5cb9c23ec7d73a2673946e257c483c6dbe2f6a12
djanagam <92891623+djanagam@users.noreply.github.com>
2024-06-22 16:55:19 -0400

    Update alerts-json-csv.sh

--- grafana/alerts-json-csv.sh
+++ grafana/alerts-json-csv.sh
@@ -18,3 +18,44 @@ jq.exe -r '
 | $keys, map([.[ $keys[] ]])[] 
 | @csv
 ' alert_rules.json > alert_rules.csv
+
+
+################
+
+jq.exe -r '
+[
+  .[] 
+  | .orgid as $orgid 
+  | .e as $title 
+  | .folder as $folder 
+  | .interval as $interval
+  | .groups[]
+  | .rules[]
+  | .uid as $uid
+  | .title as $rule_title
+  | .condition as $condition
+  | .data[]
+  | {
+      orgid: $orgid,
+      title: $title,
+      folder: $folder,
+      interval: $interval,
+      uid: $uid,
+      rule_title: $rule_title,
+      condition: $condition,
+      refid: .refid,
+      from: .relativeTimeRange.from,
+      to: .relativeTimeRange.to,
+      datasourceUid: .datasourceUid,
+      mod: .mod,
+      nin_doc_count: ."nin doc count",
+      order: .order,
+      orderBy: .orderBy,
+      size: .size,
+      type: .type
+    }
+]
+| (first | keys_unsorted) as $keys 
+| $keys, map([.[ $keys[] ]])[] 
+| @csv
+' path\to\alert_rules.json > path\to\alert_rules.csv
