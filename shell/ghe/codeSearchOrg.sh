83dfe99d7d1f5c78c6784f6d581b2ac18d451692
djanagam <92891623+djanagam@users.noreply.github.com>
2024-09-23 22:34:08 -0400

    Update codeSearchOrg.sh

--- shell/ghe/codeSearchOrg.sh
+++ shell/ghe/codeSearchOrg.sh
@@ -1,3 +1,9 @@
 curl -s -H "Authorization: token ghp_xxxxxxx" \
     "https://api.github.com/search/code?q=%22com.gradle.enterprise%22+in:file+org:app-tst&per_page=100" | \
     jq -r '.items[] | [.repository.full_name, .name, .html_url] | @csv'
+    
+    
+    
+    curl -s -H "Authorization: token ghp_xxxxxxx" \
+    "https://api.github.com/search/code?q=%22com.gradle.enterprise%22+in:file+org:app-tst&per_page=100" | \
+    jq -r '.items[] | select(.text_matches[].fragment | contains("com.gradle.enterprise")) | [.repository.full_name, .name, .html_url] | @csv'
