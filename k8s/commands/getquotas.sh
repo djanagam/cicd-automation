829b75a5bf697dfe854589cc483e549bd83841f6
djanagam <92891623+djanagam@users.noreply.github.com>
2025-02-11 15:00:34 -0500

    Create getquotas.sh

---
+++ k8s/commands/getquotas.sh
@@ -0,0 +1,1 @@
+kubectl get resourcequotas -A -o custom-columns="NAMESPACE:.metadata.namespace,NAME:.metadata.name,CPU:.status.hard.cpu,MEMORY:.status.hard.memory,PODS:.status.hard.pods,STORAGE:.status.hard['requests.storage']" --no-headers | tr -s ' ' ','
